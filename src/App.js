9e84d99acefb64774cd977291fbf5475cc776854
rachelmad <rachelvmadrigal@gmail.com>
2017-09-18 18:39:50 -0500

    Automatically logs in, added log out button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
+import $ from 'jquery';
 
 import Header from './Header.js';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  componentDidMount() {
+  constructor() {
+    super();
 
+    this.state = {
+      token: ""
+    };
+    this.logOut = this.logOut.bind(this);
   }
+
+  componentDidMount() {
+    var bodyString = "username=" + process.env.REACT_APP_USERNAME + "&password=" + process.env.REACT_APP_PASSWORD;
+
+    $.ajax({
+      type: 'POST', 
+      url: 'https://api.robinhood.com/api-token-auth/',
+      data: bodyString
+    })
+    .then(function(data) {
+      this.setState(prevState => ({
+          token: data.token
+        })
+      );
+    }.bind(this));
+  }
+
+  logOut() {
+    var tokenString = "Token " + this.state.token;
+
+    $.ajax({
+      type: 'POST', 
+      url: 'https://api.robinhood.com/api-token-logout/',
+      headers: {
+        'Authorization': tokenString
+      },
+      success: function(data) {
+        console.log(data);
+      }
+    });
+  }
+
    render() {
     return (
       <div className="App">
@@ -16,8 +54,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Button>
-          Sample
+        <Button onClick={this.logOut}>
+          Log Out
         </Button>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-console.log(process.env.REACT_APP_TEST);
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
